62387701ff40a9318eed447ec153980862fc2833
Jun Takahashi <jtakahas@princeton.edu>
2015-05-11 22:46:40 -0400

    lda

--- clustering.py
+++ clustering.py
@@ -13,32 +13,56 @@ print str(test_percentage) + "% Testing, Features: Start Station ID, Start Hour"
 
 test_x = []
 test_y = []
-train_x = []
-train_y = []
+birthyears = []
+genders = []
 
 raw_data = np.genfromtxt(raw_file, dtype=str, delimiter=',', skip_header=1)
-x_data_list = [int(r[3][1:-1]) for r in raw_data]
-y_data_list = [int(r[7][1:-1]) for r in raw_data]
 
-stations = []
-i = 0
-for x, y in zip(x_data_list, y_data_list):
-	if i%test_percentage == 0:
-		test_x.append(x)
-		test_y.append(y)
-	else:
-		train_x.append(x)
-		train_y.append(y)
-	if y not in stations:
-		stations.append(y)
-	i+=1
-train_x = np.asarray(train_x, dtype=int)
-train_y = np.asarray(train_y, dtype=int)
-test_x = np.asarray(test_x, dtype=int)
-test_y = np.asarray(test_y, dtype=int)
+for i in range(len(raw_data)):
+	if raw_data[i][13] != '\N':
+		birthyears.append(int(raw_data[i][13][1:-1]))
+		genders.append(int(raw_data[i][14][1:-1]))
+
+
+
+# raw_data = np.genfromtxt(raw_file, dtype=str, delimiter=',', skip_header=1)
+# x_data_list = [int(r[3][1:-1]) for r in raw_data]
+# y_data_list = [int(r[7][1:-1]) for r in raw_data]
+
+# stations = []
+# i = 0
+# for x, y in zip(x_data_list, y_data_list):
+# 	if i%test_percentage == 0:
+# 		test_x.append(x)
+# 		test_y.append(y)
+# 	else:
+# 		train_x.append(x)
+# 		train_y.append(y)
+# 	if y not in stations:
+# 		stations.append(y)
+# 	i+=1
+
+# train_x = np.asarray(train_x, dtype=int)
+# train_y = np.asarray(train_y, dtype=int)
+# test_x = np.asarray(test_x, dtype=int)
+# test_y = np.asarray(test_y, dtype=int)
 
 # print train_x;
 # print train_y;
 
-model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
-model.fit(train_x);
+# zipped = zip(train_x, train_y);
+
+# maxstation = max(max(train_x), max(train_y)) + 1;
+# trips = [[0]*maxstation for _ in range(maxstation)];
+
+# for x, y in zipped:
+# 	trips[x][y] += 1;
+# 	trips[y][x] += 1;
+
+# nonzeros = 0;
+# for i in range(maxstation):
+# 	for j in range(maxstation):
+# 		if (trips[i][j] != 0):
+# 			nonzeros += 1;
+
+# sorted(trips);
